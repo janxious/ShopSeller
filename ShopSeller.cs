a0e9ab81e8603818528efd7aea07c134ad3802ad
Joel Meador <joel@expectedbehavior.com>
2018-05-29 11:53:29 -0400

    add functioning <modifier>+enter buy/sell, and fix bug in base game around buying with enter key while in flight

--- ShopSeller.cs
+++ ShopSeller.cs
@@ -34,39 +34,48 @@ namespace ShopSeller
             }
         }
 
-        // The following code doesn't work in the sense that
-        // the the only key that seems to be held down is the enter
-        // when we get in the code. The why of that is somewhat a mystery
-        // to me.
-//        [HarmonyPatch(typeof(SG_Shop_Screen), "HandleEnterKeypress")]
-//        public static class SG_Shop_HandleEnterKeypressPatch
-//        {
-//            static bool Prefix(
-//                ref bool __result,
-//                SG_Shop_Screen __instance
-//            )
-//            {
-//                Logger.Debug("We hit the handle enter patch");
-//                var shiftHeld = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
-//                var ctrlHeld = Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl);
-//                var ctrlAndShiftHeld = shiftHeld && ctrlHeld;
-//                Logger.Debug($"shift held: {shiftHeld}\nctrl held: {ctrlHeld}\nctrlshift held: {ctrlAndShiftHeld}");
-//                var btn = Traverse.Create(__instance).Field("BuyBotton").GetValue<HBSButton>();
-//                var btnNotVisible = !(btn != null) || 
-//                                    !__instance.Visible ||
-//                                    !btn.gameObject.activeInHierarchy || 
-//                                    btn.State != ButtonState.Enabled;
-//                if (btnNotVisible)
-//                {
-//                    __result = false;
-//                    return false;
-//                }
-//                __result = true;
-//                __instance.ReceiveButtonPress(btn.name);
-//                return false;
-//            }
-//        }
+        // By default, the game doesn't mind any key combos with Enter, so
+        // we need to keep the old binding and add some new ones to handle:
+        // enter
+        // shift+enter
+        // ctrl+enter
+        // ctrl+shift+enter
+        [HarmonyPatch(typeof(StaticActions), "CreateWithDefaultBindings")]
+        public static class StaticActions_Patch_CreateWDB
+        {
+            public static void Postfix(ref StaticActions __result)
+            {
+                Logger.Debug("Clearing Default Bindings for enter Key");
+                __result.Return.ClearBindings();
+                Logger.Debug("Setting enter and others key as defaults for enter Key");
+                __result.Return.AddDefaultBinding(new Key[]
+                {
+                    Key.Return
+                });
+                __result.Return.AddDefaultBinding(new Key[]
+                {
+                    Key.Shift,
+                    Key.Return
+                });
+                __result.Return.AddDefaultBinding(new Key[]
+                {
+                    Key.RightShift,
+                    Key.Return
+                });
+                __result.Return.AddDefaultBinding(new Key[]
+                {
+                    Key.LeftShift,
+                    Key.Return
+                });
+                Logger.Debug("Success setting enter and others key as defaults for enter Key");
+            }
+        }
 
+        // Override how the game deals with the "Captialism" button press.
+        // That button is the Buy/Sell button (in english).
+        // The enter key presses above route to this as well.
+        // Also this fixes a bug in the base game wherein enter would get
+        // around travel restrictions for buying and selling.
         [HarmonyPatch(typeof(SG_Shop_Screen), "ReceiveButtonPress")]
         public static class SG_Shop_ReceiveButtonPressPatch
         {
@@ -79,30 +88,37 @@ namespace ShopSeller
                 SG_Shop_Screen __instance)
             {
                 shopScreen = __instance;
+                var selectedController = Traverse.Create(shopScreen).Field("selectedController").GetValue<ListElementController_BASE>();
+                var selectedControllerIsPresent = selectedController != null;
+                var isBuySellButton = button == "Capitalism";
+                if (!isBuySellButton || !selectedControllerIsPresent)
+                {
+                    Logger.Debug($"buysellbutton: {isBuySellButton}\nselectedController: {selectedControllerIsPresent}");
+                    ResetVariables();
+                    return true;
+                }
+                var simGameState = Traverse.Create(shopScreen).Field("simState").GetValue<SimGameState>();
+                var isTraveling = simGameState.TravelState != SimGameTravelStatus.IN_SYSTEM;
+                if (isTraveling)
+                {
+                    Logger.Debug("traveling, so block buy/sell");
+                    ResetVariables();
+                    return false;
+                }
                 var shiftHeld = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
                 var ctrlHeld = Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl);
                 var ctrlAndShiftHeld = shiftHeld && ctrlHeld;
                 Logger.Debug($"shift held: {shiftHeld}\nctrl held: {ctrlHeld}\nctrlshift held: {ctrlAndShiftHeld}");
-                var selectedController = Traverse.Create(__instance).Field("selectedController").GetValue<ListElementController_BASE>();
-                var selectedControllerIsPresent = selectedController != null;
-                var isInBuyingState = Traverse.Create(__instance).Field("isInBuyingState").GetValue<bool>();
-                var isBuySellButton = button == "Capitalism";
-                if (!isBuySellButton || !selectedControllerIsPresent)
-                {
-                    Logger.Debug("Different button, no need to do anything");
-                    ResetVariables();
-                    return true;
-                }
-
-                var amounts = new List<int>
+                var isInBuyingState = Traverse.Create(shopScreen).Field("isInBuyingState").GetValue<bool>();
+                var activeModifierAmounts = new List<int>
                 {
                     ModSettings.CtrlAndShiftKeyCombinationModifierActive && ctrlAndShiftHeld ? ModSettings.CtrlAndShiftKeyCombinationAmount : 0,
                     ModSettings.CtrlKeyModifierActive && ctrlHeld ? ModSettings.CtrlKeyAmount : 0,
                     ModSettings.ShiftKeyModifierActive && shiftHeld ? ModSettings.ShiftKeyAmount : 0,
                     1 // no modifier keys
                 };
-                var maxModifierAmount = amounts.Max();
-                selectedController.RefreshInfo();
+                var maxModifierAmount = activeModifierAmounts.Max();
+                // selectedController.RefreshInfo();
                 var shopDefItem = selectedController.shopDefItem;
                 var quantity = shopDefItem.IsInfinite ? BigNumberForApproxInfinity : selectedController.quantity;
                 Logger.Debug($"raw count: {selectedController.quantity}");
@@ -111,9 +127,9 @@ namespace ShopSeller
                 Logger.Debug($"how many? {numberToTrade}");
                 numToBuyOrSell = numberToTrade;
 
-                var simGameState = Traverse.Create(__instance).Field("simState").GetValue<SimGameState>();
+
                 var minimumThreshold = simGameState.Constants.Finances.ShopWarnBeforeSellingPriceMinimum;
-                amounts.ForEach(delegate(int amount) { Logger.Debug($"{amount}"); });
+                activeModifierAmounts.ForEach(delegate(int amount) { Logger.Debug($"{amount}"); });
                 Logger.Debug($"max: {numToBuyOrSell}");
                 Logger.Debug($"threshold: {minimumThreshold}");
 
@@ -161,34 +177,32 @@ namespace ShopSeller
                 }
             }
 
-            static void ResetVariables()
-            {
-                numToBuyOrSell = 0;
-                shopScreen = null; // clear ref for GC
-            }
-
-            static void BuyCurrent()
+            private static void BuyCurrent()
             {
                 Logger.Debug($"buying: {numToBuyOrSell}");
                 while (numToBuyOrSell > 0)
                 {
-                    Logger.Debug($"{numToBuyOrSell}");
                     numToBuyOrSell--;
                     shopScreen.BuyCurrentSelection();
                 }
                 ResetVariables();
             }
 
-            static void SellCurrent()
+            private static void SellCurrent()
             {
                 Logger.Debug($"selling: {numToBuyOrSell}");
                 while (numToBuyOrSell > 0)
                 {
-                    Logger.Debug($"{numToBuyOrSell}");
                     numToBuyOrSell--;
                     shopScreen.SellCurrentSelection();
                 }
                 ResetVariables();
+            }
+
+            private static void ResetVariables()
+            {
+                numToBuyOrSell = 0;
+                shopScreen = null; // clear ref for GC
             }
         }
     }
